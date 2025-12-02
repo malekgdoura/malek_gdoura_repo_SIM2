9f9f084ecec1ea34017df3c5d4106d744bed7cbf
malekgdoura <148217985+malekgdoura@users.noreply.github.com>
2025-12-02 21:44:41 +0100

    Update StudentService.java

--- src/main/java/tn/esprit/studentmanagement/services/StudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/StudentService.java
@@ -1,7 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.repositories.StudentRepository;
@@ -10,11 +8,30 @@ import java.util.List;
 
 @Service
 public class StudentService implements IStudentService {
-    @Autowired
-    private StudentRepository studentRepository;
-    public List<Student> getAllStudents() { return studentRepository.findAll(); }
-    public Student getStudentById(Long id) { return studentRepository.findById(id).orElse(null); }
-    public Student saveStudent(Student student) { return studentRepository.save(student); }
-    public void deleteStudent(Long id) { studentRepository.deleteById(id); }
 
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
 }
