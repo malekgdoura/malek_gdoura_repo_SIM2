3de3ef179f8d55e543004b29216fbc12771b2447
malekgdoura <148217985+malekgdoura@users.noreply.github.com>
2025-12-03 20:05:21 +0100

    Update StudentService.java

--- src/main/java/tn/esprit/studentmanagement/services/StudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/StudentService.java
@@ -1,6 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Student;
@@ -10,7 +9,6 @@ import java.util.List;
 
 @Service
 public class StudentService implements IStudentService {
-    @Autowired
     private StudentRepository studentRepository;
     public List<Student> getAllStudents() { return studentRepository.findAll(); }
     public Student getStudentById(Long id) { return studentRepository.findById(id).orElse(null); }
