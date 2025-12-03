2008faf6d79f705f7a34cb27e7ec38296e43a703
malekgdoura <148217985+malekgdoura@users.noreply.github.com>
2025-12-03 18:59:56 +0100

    Update DepartmentService.java

--- src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
@@ -1,35 +1,34 @@
 package tn.esprit.studentmanagement.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tn.esprit.studentmanagement.entities.Department;
-import tn.esprit.studentmanagement.repositories.DepartmentRepository;
-
+import tn.esprit.studentmanagement.repositories.EnrollmentRepository;
+import tn.esprit.studentmanagement.entities.Enrollment;
 import java.util.List;
 
 @Service
-
-public class DepartmentService implements IDepartmentService {
+public class EnrollmentService implements IEnrollment {
     @Autowired
-    DepartmentRepository departmentRepository;
+    EnrollmentRepository enrollmentRepository;
 
     @Override
-    public List<Department> getAllDepartments() {
-        return departmentRepository.findAll();
+    public List<Enrollment> getAllEnrollments() {
+        return enrollmentRepository.findAll();
     }
 
     @Override
-    public Department getDepartmentById(Long idDepartment) {
-        return departmentRepository.findById(idDepartment).get();
+    public Enrollment getEnrollmentById(Long idEnrollment) {
+        return enrollmentRepository.findById(idEnrollment).get();
     }
 
     @Override
-    public Department saveDepartment(Department department) {
-        return departmentRepository.save(department);
+    public Enrollment saveEnrollment(Enrollment enrollment) {
+        return enrollmentRepository.save(enrollment);
     }
 
     @Override
-    public void deleteDepartment(Long idDepartment) {
-departmentRepository.deleteById(idDepartment);
+    public void deleteEnrollment(Long idEnrollment) {
+enrollmentRepository.deleteById(idEnrollment);
     }
 }
