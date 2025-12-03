e2c454c9c34bdb9cd4fb6bbea3ad1e066229a97b
malekgdoura <148217985+malekgdoura@users.noreply.github.com>
2025-12-03 20:06:57 +0100

    Update DepartmentService.java

--- src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 
 public class DepartmentService implements IDepartmentService {
-    @Autowired
+    
     DepartmentRepository departmentRepository;
 
     @Override
@@ -20,7 +20,7 @@ public class DepartmentService implements IDepartmentService {
 
     @Override
     public Department getDepartmentById(Long idDepartment) {
-        return departmentRepository.findById(idDepartment).get();
+        return departmentRepository.findById(idDepartment).orElse(null);
     }
 
     @Override
